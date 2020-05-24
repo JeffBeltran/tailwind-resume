5d51eb218d422e88c06074562d8a1ba1209bb0c0
Jeff Beltran <jeffbeltran@gmail.com>
2020-05-23 21:11:56 -0700

    General Pages created

---
+++ pages/knowledge-cloud.js
@@ -0,0 +1,20 @@
+import { BasePage } from "../components";
+import { useRouter } from "next/router";
+
+export default function KnowledgeCloud() {
+  const router = useRouter();
+
+  return (
+    <>
+      <BasePage pageTitle="Knowledge Cloud" activeUrl={router.pathname}>
+        <div className="px-4 py-8 sm:px-0">
+          <div className="border-4 border-gray-200 border-dashed rounded-lg h-96">
+            Pellentesque purus aptent quisque arcu laoreet adipiscing senectus,
+            tempus donec condimentum potenti justo mattis sit, posuere malesuada
+            quam natoque turpis sollicitudin.
+          </div>
+        </div>
+      </BasePage>
+    </>
+  );
+}
---
+++ pages/resume.js
@@ -0,0 +1,20 @@
+import { BasePage } from "../components";
+import { useRouter } from "next/router";
+
+export default function Resume() {
+  const router = useRouter();
+
+  return (
+    <>
+      <BasePage pageTitle="Resume" activeUrl={router.pathname}>
+        <div className="px-4 py-8 sm:px-0">
+          <div className="border-4 border-gray-200 border-dashed rounded-lg h-96">
+            Pellentesque purus aptent quisque arcu laoreet adipiscing senectus,
+            tempus donec condimentum potenti justo mattis sit, posuere malesuada
+            quam natoque turpis sollicitudin.
+          </div>
+        </div>
+      </BasePage>
+    </>
+  );
+}
