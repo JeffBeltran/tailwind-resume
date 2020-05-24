1e6643acca0d20d00e733d50a31b2194f29652ae
Jeff Beltran <jeffbeltran@gmail.com>
2020-05-23 19:32:16 -0700

    Basic Footer added

--- components/index.js
+++ components/index.js
@@ -1,6 +1,7 @@
 export { default as Hero } from "./hero";
 export { default as FAQ } from "./faq";
 export { default as CTA } from "./cta";
+export { default as Footer } from "./footer";
 export { default as HeroMainNav } from "./hero-main-nav";
 export { default as HeroMobileNav } from "./hero-mobile-nav";
 export { default as Transition } from "./transition";
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import "../styles/index.css";
+import { Footer } from "../components";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Footer></Footer>
+    </>
+  );
 }
 
 export default MyApp;
