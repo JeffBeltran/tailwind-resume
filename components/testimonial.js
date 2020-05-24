a01bdb51b88223e41839a851960b47e40dc92690
Jeff Beltran <jeffbeltran@gmail.com>
2020-05-23 18:30:41 -0700

    fix mobile image on testimonial

--- components/testimonial.js
+++ components/testimonial.js
@@ -98,7 +98,7 @@ function Testimonial({ children, imgSrc, name, who, from }) {
                   <div className="flex-shrink-0 lg:hidden">
                     <img
                       className="w-12 h-12 rounded-full"
-                      src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                      src={imgSrc}
                       alt=""
                     />
                   </div>
